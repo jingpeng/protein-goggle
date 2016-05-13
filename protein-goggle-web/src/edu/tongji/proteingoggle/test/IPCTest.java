9662c816bddce1ee52b215081934ac1901750fe4
王劲鹏 <jingpeng.wang@outlook.com>
2016-05-13 12:55:59 +0800

    add cmd args

--- protein-goggle-web/src/edu/tongji/proteingoggle/test/IPCTest.java
+++ protein-goggle-web/src/edu/tongji/proteingoggle/test/IPCTest.java
@@ -9,7 +9,7 @@ public class IPCTest {
 	public static void main(String[] args) {
 		CreateDatabase cdb = new CreateDatabase();
 		try {
-			cdb.Excute(new File("D:/P83570.txt"));
+			cdb.Excute(new File(args[0]));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
